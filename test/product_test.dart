849606672e56398315da4e90bca0cdf55eff43b7
Joyeehing <hingsiyun@gmail.com>
2025-12-05 12:40:14 +0000

    remove unsued import

--- test/product_test.dart
+++ test/product_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:union_shop/product_page.dart';
 
 void main() {
   group('Product Page Tests', () {
